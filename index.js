20768cd6d68b6387d0f60b40404edb975ec72b2d
sablinali <sablinahare@gmail.com>
2024-04-08 22:43:47 +0200

    Created a memory game to play with colors.

---
+++ index.js
@@ -0,0 +1,114 @@
+const colors = ["red", "blue", "yellow", "green"];
+let shuffledCards = [];
+let turn = 0;
+
+function playWrongSound() {
+  let wrong = new Audio("audios/Buzzer-sound.mp3");
+  wrong.play();
+}
+
+function playRightSound() {
+  let correct = new Audio("audios/Right-answer-ding-ding-sound-effect.mp3");
+  correct.play();
+}
+
+function playWeeSound() {
+  let wee = new Audio("audios/wee-sound-effect.mp3");
+  wee.play();
+}
+
+function generateRandomIndex(max) {
+  //This will generate a number between 0 and max
+  return Math.floor(Math.random() * max);
+}
+
+function sortColors() {
+  return colors.sort(() => Math.random() - 0.5);
+}
+
+function displayAndFadeColors() {
+  shuffledCards = sortColors();
+  console.log(shuffledCards);
+
+  for (let i = 0; i < shuffledCards.length; i++) {
+    let squareEl = document.querySelector(`#${shuffledCards[i]}`);
+
+    setTimeout(() => {
+      squareEl.style.backgroundColor = shuffledCards[i];
+    }, i * 1000);
+
+    setTimeout(() => {
+      squareEl.style.backgroundColor = "white";
+    }, i * 1000 + 500);
+  }
+}
+
+function checkColors(turn, clickedColor) {
+  if (clickedColor === shuffledCards[turn]) {
+    playRightSound();
+
+    if (turn === shuffledCards.length - 1) {
+      playWeeSound();
+      alert("You won!");
+      setTimeout(() => {}, 3000);
+    }
+  } else {
+    playWrongSound();
+    alert("You lost!");
+    // displayAndFadeColors();
+  }
+}
+
+function makeAllSquaresWhite() {
+  const squares = document.querySelectorAll("#red, #blue , #yellow , #green");
+
+  for (let i = 0; i < squares.length; i++) {
+    console.log(squares[i]);
+    squares[i].style.backgroundColor = "white";
+  }
+}
+
+const redSquareEl = document.querySelector("#red");
+const blueSquareEl = document.querySelector("#blue");
+const yellowSquareEl = document.querySelector("#yellow");
+const greenSquareEl = document.querySelector("#green");
+const startButtonEl = document.querySelector("button");
+
+redSquareEl.addEventListener("click", () => {
+  alert("red");
+});
+blueSquareEl.addEventListener("click", () => {
+  alert("blue");
+});
+yellowSquareEl.addEventListener("click", () => {
+  alert("yellow");
+});
+greenSquareEl.addEventListener("click", () => {
+  alert("green");
+});
+startButtonEl.addEventListener("click", () => makeAllSquaresWhite());
+
+redSquareEl.addEventListener("click", () => {
+  checkColors(turn, "red");
+  turn++;
+});
+
+blueSquareEl.addEventListener("click", () => {
+  checkColors(turn, "blue");
+  turn++;
+});
+
+yellowSquareEl.addEventListener("click", () => {
+  checkColors(turn, "yellow");
+  turn++;
+});
+
+greenSquareEl.addEventListener("click", () => {
+  checkColors(turn, "green");
+  turn++;
+});
+
+startButtonEl.addEventListener("click", () => {
+  makeAllSquaresWhite();
+  displayAndFadeColors();
+});
